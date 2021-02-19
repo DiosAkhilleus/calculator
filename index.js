39772cd619ca794ac3b5d0fc9fe21bc958e8f5c8
DiosAkhilleus <abertin1809@gmail.com>
2021-02-19 11:18:38 -0600

    added a few calc functions, changed button styling

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-console.log("wtf");
-
-// function calcDivs(){
-    
-//     for(let i = 0; i < 16; i++){
-//     let x = document.createElement("DIV");
-//     x.setAttribute('id', 'button');
-//     x.setAttribute('line-height', '400px');
-//     x.innerHTML=i;
-//     document.getElementById("calc-container").appendChild(x);
-//     }
-// }
-
-
 function add (x, y) {
     let result = x + y;
+    console.log(result);
+    return result;
+}
+
+function subtract (x, y) {
+    let result = x - y;
+    console.log(result);
+    return result;
+}
+
+function multiply (x, y) {
+    let result = x * y;
+    console.log(result);
+    return result;
+}
+
+function divide (x, y) {
+    let result = x / y;
     console.log(result);
     return result;
 }
