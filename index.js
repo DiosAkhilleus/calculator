0c302c31809cc2244600031dfd73c094aa173aff
DiosAkhilleus <abertin1809@gmail.com>
2021-02-19 11:06:27 -0600

    initial calculator setup - still need to figure out proper button layout

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 console.log("wtf");
 
-function calcDivs(){
+// function calcDivs(){
     
-    for(let i = 0; i < 16; i++){
-    let x = document.createElement("DIV");
-    x.setAttribute('id', 'button');
-    x.setAttribute('line-height', '400px');
-    x.innerHTML=i;
-    document.getElementById("calc-container").appendChild(x);
-    }
+//     for(let i = 0; i < 16; i++){
+//     let x = document.createElement("DIV");
+//     x.setAttribute('id', 'button');
+//     x.setAttribute('line-height', '400px');
+//     x.innerHTML=i;
+//     document.getElementById("calc-container").appendChild(x);
+//     }
+// }
+
+
+function add (x, y) {
+    let result = x + y;
+    console.log(result);
+    return result;
 }
