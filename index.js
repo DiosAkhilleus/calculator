ae5c05105b4c9231f2cd51cc67b512ca0aa928d2
DiosAkhilleus <abertin1809@gmail.com>
2021-02-18 21:44:11 -0600

    Started experimenting

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+console.log("wtf");
+
+function calcDivs(){
+    
+    for(let i = 0; i < 16; i++){
+    let x = document.createElement("DIV");
+    x.setAttribute('id', 'button');
+    x.setAttribute('line-height', '400px');
+    x.innerHTML=i;
+    document.getElementById("calc-container").appendChild(x);
+    }
+}
