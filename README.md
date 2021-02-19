9ff65c7c4c8658129ddf3be6f0d36938d8795541
DiosAkhilleus <abertin1809@gmail.com>
2021-02-19 12:52:54 -0600

    calc mostly done! still have to fix calc overflow when using large numbers

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ Makin' a JS Calculator!
 
 Should have all normal 4-function calculator abilities!
 
+Intending to program it with RPN (Reverse Polish Notation)
+    - This works slightly differently than a normal 4-func calculator.
+    
+Order of Operations:
+    1. Type first number
+    2. Press 'enter' (this sets the register to whatever number you previously entered)
+    3. Type second number
+    4. Choose operator
+    5. Result should display
+
 --DiosAkhilleus
--- index.js
+++ index.js
@@ -1,23 +1,75 @@
-function add (x, y) {
-    let result = x + y;
-    console.log(result);
-    return result;
+let nums = document.getElementById('num');
+let store = document.getElementById('numline').innerHTML;
+let reg = document.getElementById('register').innerHTML;
+
+function init () {
+    document.getElementById('numline').innerHTML = '0';
+    store = 0;
 }
 
-function subtract (x, y) {
-    let result = x - y;
+function add () {
+    let num1 = document.getElementById('register').innerHTML;
+    let num2 = document.getElementById('numline').innerHTML;
+    let result = +num1 + +num2;
+    document.getElementById('register').innerHTML = '';
+    document.getElementById('numline').innerHTML = result;
     console.log(result);
-    return result;
 }
 
-function multiply (x, y) {
-    let result = x * y;
+function subtract () {
+    let num1 = document.getElementById('register').innerHTML;
+    let num2 = document.getElementById('numline').innerHTML;
+    let result = +num1 - +num2;
+    document.getElementById('register').innerHTML = '';
+    document.getElementById('numline').innerHTML = result;
     console.log(result);
-    return result;
 }
 
-function divide (x, y) {
-    let result = x / y;
+function multiply () {
+    let num1 = document.getElementById('register').innerHTML;
+    let num2 = document.getElementById('numline').innerHTML;
+    let result = +num1 * +num2;
+    document.getElementById('register').innerHTML = '';
+    document.getElementById('numline').innerHTML = result;
     console.log(result);
-    return result;
+}
+
+function divide () {
+    let num1 = document.getElementById('register').innerHTML;
+    let num2 = document.getElementById('numline').innerHTML;
+    let result = +num1 / +num2;
+    document.getElementById('register').innerHTML = '';
+    document.getElementById('numline').innerHTML = result;
+    console.log(result);
+}
+
+function clr() {
+    console.log("clear");
+    let x = document.getElementById('numline');
+    let y = document.getElementById('register');
+    x.innerHTML = '0';
+    y.innerHTML = '';
+}
+
+
+function setNum() {
+    let x = this.innerHTML;
+    let y = document.getElementById('numline').innerHTML;
+    
+    if(y.length < 9){
+        if(y === '0'){
+            document.getElementById('numline').innerHTML = x;
+        }
+        if (y !== '0'){
+            document.getElementById('numline').innerHTML += x;
+        }
+    }
+}
+
+function setReg() {
+    store = document.getElementById('numline');
+    reg = document.getElementById('register');
+    document.getElementById('register').innerHTML = store.innerHTML;
+    document.getElementById('numline').innerHTML = '0';
+    console.log(store.innerHTML, reg.innerHTML);
 }
